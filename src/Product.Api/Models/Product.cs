d3eb5f11a48ca540b57b0f056c0a88efbf64eaf6
Miguel Marin <miguel91.macias@gmail.com>
2025-03-12 19:28:38 -0700

    Added the Product class

---
+++ src/Product.Api/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace Product.Api.Models
+{
+    /// <summary>
+    /// Represents a product item available in the store.
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// The unique identifier of the product.
+        /// </summary>
+        public long Id { get; set; }    
+
+        /// <summary>
+        /// The product's description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The current retail price of the item.
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// The current amount of units available in the store.
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
