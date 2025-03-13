e638f70dce0b8405f013679057dceb78c0b501a2
Miguel Marin <miguel91.macias@gmail.com>
2025-03-12 20:17:41 -0700

    Added the concrete implementation for the product inventory system

---
+++ src/Domain/Implementations/ProductInventory.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Abstractions;
+using Domain.Models;
+
+namespace Domain.Implementations
+{
+    internal class ProductInventory : IProductInventory
+    {
+        /// <inheritdoc />
+        public Task AddProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task DeleteProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task RetrieveAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task UpdateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
