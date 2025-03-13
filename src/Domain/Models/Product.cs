88100023006d6c52a1ef1b9733df65622f25c7b3
Miguel Marin <miguel91.macias@gmail.com>
2025-03-12 20:09:13 -0700

    Added the Product domain model

---
+++ src/Domain/Models/Product.cs
@@ -0,0 +1,51 @@
+﻿namespace Domain.Models
+{
+    /// <summary>
+    /// Represents a product item available in the store.
+    /// </summary>
+    internal class Product
+    {
+        public Product(long productId, string description, decimal price, int quantity)
+        {
+            if (productId <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(productId), "The product's Id must be larger than 0.");
+            }
+
+            if (price <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(productId), "The product's current price must be larger than 0.");
+            }
+
+            if (quantity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(productId), "The product's quantity cannot be negative.");
+            }
+
+            Id = productId;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        /// <summary>
+        /// The unique identifier of the product.
+        /// </summary>
+        public long Id { get; init; }
+
+        /// <summary>
+        /// The product's description.
+        /// </summary>
+        public string Description { get; init; } = default!;
+
+        /// <summary>
+        /// The current retail price of the item.
+        /// </summary>
+        public decimal Price { get; init; } 
+
+        /// <summary>
+        /// The current amount of units available in the store.
+        /// </summary>
+        public int Quantity { get; init; }
+    }
+}
