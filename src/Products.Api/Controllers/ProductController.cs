185be86887af54dc9c061f90ab1ad5404e743a97
Miguel Marin <miguel91.macias@gmail.com>
2025-03-12 19:52:20 -0700

    Enhanced the products controller method

--- src/Products.Api/Controllers/ProductController.cs
+++ src/Products.Api/Controllers/ProductController.cs
@@ -31,6 +31,17 @@ namespace Products.Api.Controllers
             })
             .ToArray();
 
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unknown error occurred while retrieving all of the store's products.");
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
             return Ok(allProducts);
         }
 
@@ -38,19 +49,61 @@ namespace Products.Api.Controllers
         public IActionResult AddProduct([FromBody] Product product)
         {
 
-            _logger.LogInformation("Atttempting to retrieve all of the store products.");
+            _logger.LogInformation("Atttempting to add a product.");
 
-            var allProducts = Enumerable.Range(1, 5).Select(index => new Product
+            try
             {
-                Id = index,
-                Description = "Some Description",
-                Price = 1,
-                Quantity = 10
 
-            })
-            .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unknown error occurred while adding a product to the system.");
 
-            return Ok(allProducts);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut(Name = "UpdateProduct")]
+        public IActionResult UpdateProduct([FromBody] Product product)
+        {
+
+            _logger.LogInformation("Atttempting to update a product.");
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unknown error occurred while updating an existing product.");
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut(Name = "DeleteProduct")]
+        public IActionResult DeleteProduct([FromBody] long productId)
+        {
+
+            _logger.LogInformation($"Atttempting to delete a product with the following id: {productId}");
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unknown error occurred while deleting the product from the system.");
+               
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+
+            return Ok();
         }
     }
 }
