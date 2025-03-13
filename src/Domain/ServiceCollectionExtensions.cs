7a61726f0585006396ff2fd83f1987f74f6e8f97
Miguel Marin <miguel91.macias@gmail.com>
2025-03-12 20:27:39 -0700

    Registered the IProductInventory service

---
+++ src/Domain/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Abstractions;
+using Domain.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Domain
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Registers the product inventory service in the dependency injection container.
+        /// </summary>
+        /// <param name="services">The IServiceCollection instance to which the service is added.</param>
+        /// <returns>The updated IServiceCollection instance.</returns>
+        public static IServiceCollection RegisterTheProductInventory(this IServiceCollection services)
+        {
+            return services.AddScoped<IProductInventory, ProductInventory>();
+        }
+
+    }
+}
--- src/Products.Api/Program.cs
+++ src/Products.Api/Program.cs
@@ -1,6 +1,10 @@
+using Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.RegisterTheProductInventory();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
