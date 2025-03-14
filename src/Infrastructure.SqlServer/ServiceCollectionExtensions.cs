2457392517be628141f96ba9790cbda3479c6016
Miguel Marin <miguel91.macias@gmail.com>
2025-03-14 15:50:41 -0700

    Added method that is going to register the db repository services

--- src/Infrastructure.SqlServer/ServiceCollectionExtensions.cs
+++ src/Infrastructure.SqlServer/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Abstractions;
+using Domain.Abstractions.Repositories;
+using Infrastructure.SqlServer.Implementations.Repositories;
 using Infrastructure.SqlServer.Mappers;
 using Infrastructure.SqlServer.Models;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,13 +11,27 @@ namespace Infrastructure.SqlServer
     public static class ServiceCollectionExtensions
     {
         /// <summary>
+        /// Registers product repository services in the dependency injection container.
+        /// </summary>
+        /// <param name="services">The service collection to add the repositories to.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection RegisterProductRepositories(this IServiceCollection services)
+        {
+            return services.AddScoped<IReadProducts, ProductReader>()
+                .AddScoped<IRemoveProducts, ProductRemover>()
+                .AddScoped<IWriteProducts, ProductWriter>()
+                .RegisterDbProductMapper();
+        }
+
+        /// <summary>
         /// Registers the product db mapper in the dependency injection container.
         /// </summary>
         /// <param name="services">The IServiceCollection instance to which the service is added.</param>
         /// <returns>The updated IServiceCollection instance.</returns>
-        public static IServiceCollection RegisterDbProductMapper(this IServiceCollection services)
+        private static IServiceCollection RegisterDbProductMapper(this IServiceCollection services)
         {
             return services.AddScoped<IMap<Product, DomainProduct>, ProductMapper>();
         }
+
     }
 }
