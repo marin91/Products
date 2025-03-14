5b0071c75654416af01588992bf42b175182ece1
Miguel Marin <miguel91.macias@gmail.com>
2025-03-14 16:28:23 -0700

    Documented the Options class

--- src/Infrastructure.SqlServer/Options/ProductsConnectionOptions.cs
+++ src/Infrastructure.SqlServer/Options/ProductsConnectionOptions.cs
@@ -1,9 +1,18 @@
 ﻿namespace Infrastructure.SqlServer.Options
 {
+    /// <summary>
+    /// Represents the configuration options for connecting to the product database.
+    /// </summary>
     public class ProductsConnectionOptions
     {
+        /// <summary>
+        /// The configuration section name used to retrieve database connection settings.
+        /// </summary>
         public const string ConfigSection = "ProductDatabaseConnection";
 
+        /// <summary>
+        /// Gets or sets the connection string used to connect to the product database.
+        /// </summary>
         public string ConnectionString { get; set; } = default!;
     }
 }
