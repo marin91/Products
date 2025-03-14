70d12ef4a1d9bd87b1f40e55317c37e4a64ca3a0
Miguel Marin <miguel91.macias@gmail.com>
2025-03-14 14:15:09 -0700

    Added two abstractions for interacting with the product data store

---
+++ src/Domain/Abstractions/IReadProducts.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Domain.Abstractions
+{
+    internal interface IReadProducts
+    {
+        public IEnumerable<Product> RetrieveAllStoreProductsAsync();
+
+        public Product GetStoreProductByIdAsync(int id);
+    }
+}
---
+++ src/Domain/Abstractions/IWriteProducts.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+
+namespace Domain.Abstractions
+{
+    internal interface IWriteProducts
+    {
+        public Task CreateProductAsync(Product product);
+
+        public Task UpdateProductAsync(Product product);
+
+        public Task DeleteProductAsync(long productId);
+
+    }
+}
