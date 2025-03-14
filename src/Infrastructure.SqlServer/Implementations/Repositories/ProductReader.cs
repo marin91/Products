9e9ca8e8d69cc88260df283d62b8fb2dcc70ce55
Miguel Marin <miguel91.macias@gmail.com>
2025-03-14 14:24:51 -0700

    Added the product repositories

---
+++ src/Infrastructure.SqlServer/Implementations/Repositories/ProductReader.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Abstractions;
+using Domain.Models;
+
+namespace Infrastructure.SqlServer.Implementations.Repositories
+{
+    internal class ProductReader : IReadProducts
+    {
+        /// <inheritdoc />
+        public Product GetStoreProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<Product> RetrieveAllStoreProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Infrastructure.SqlServer/Implementations/Repositories/ProductWriter.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Abstractions;
+using Domain.Models;
+
+namespace Infrastructure.SqlServer.Implementations.Repositories
+{
+    internal class ProductWriter : IWriteProducts
+    {
+        /// <inheritdoc />
+        public Task CreateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task DeleteProductAsync(long productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task UpdateProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
