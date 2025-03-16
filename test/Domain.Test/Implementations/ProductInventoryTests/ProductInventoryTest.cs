54539f4371f1b5270e07936924c8f1e214447d00
Miguel Marin <miguel91.macias@gmail.com>
2025-03-16 10:58:58 -0700

    Enhanced the text fixtures

--- test/Domain.Test/Implementations/ProductInventoryTests/ProductInventoryTest.cs
+++ test/Domain.Test/Implementations/ProductInventoryTests/ProductInventoryTest.cs
@@ -1,13 +1,35 @@
-﻿namespace Domain.Test.Implementations.ProductInventoryTests
+﻿using Domain.Abstractions.Repositories;
+using Domain.Implementations;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Domain.Test.Implementations.ProductInventoryTests
 {
     internal abstract class ProductInventoryTest
     {
+        protected ProductInventory _systemUnderTest;
 
+        protected Mock<IReadProducts> _mockProductReader;
+
+        protected Mock<IWriteProducts> _mockProductWriter;
+
+        protected Mock<IRemoveProducts> _mockProductRemover;
+
+        protected Mock<ILogger<ProductInventory>> _mockLogger;
 
         [SetUp]
         public void SetUp()
         {
 
+            _mockProductReader = new Mock<IReadProducts>();
+            _mockProductWriter = new Mock<IWriteProducts>();
+            _mockProductRemover = new Mock<IRemoveProducts>();
+            _mockLogger = new Mock<ILogger<ProductInventory>>();
+
+            _systemUnderTest = new ProductInventory(_mockProductReader.Object, 
+                _mockProductWriter.Object, 
+                _mockProductRemover.Object, 
+                _mockLogger.Object);
         }
 
     }
--- test/Domain.Test/Implementations/ProductInventoryTests/When_DeleteProductAsync_Is_Called.cs
+++ test/Domain.Test/Implementations/ProductInventoryTests/When_DeleteProductAsync_Is_Called.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Test.Implementations.ProductInventoryTests
+﻿namespace Domain.Test.Implementations.ProductInventoryTests
 {
-    internal class When_DeleteProductAsync_Is_Called
+    internal class When_DeleteProductAsync_Is_Called : ProductInventoryTest
     {
 
     }
