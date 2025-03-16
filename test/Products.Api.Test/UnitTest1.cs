c6b2f956bf0a74c3656eaee943ce0d8728914915
Miguel Marin <miguel91.macias@gmail.com>
2025-03-15 23:31:20 -0700

    Added two test projects

---
+++ test/Products.Api.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Products.Api.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
