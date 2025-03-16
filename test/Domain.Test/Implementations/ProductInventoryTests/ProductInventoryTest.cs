03d90d30baf17f8911b3ec65573a55bcf3700e63
Miguel Marin <miguel91.macias@gmail.com>
2025-03-15 23:36:50 -0700

    Added test fixture classes for the Domain project

---
+++ test/Domain.Test/Implementations/ProductInventoryTests/ProductInventoryTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Test.Implementations.ProductInventoryTests
+{
+    internal abstract class ProductInventoryTest
+    {
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+    }
+}
---
+++ test/Domain.Test/Implementations/ProductInventoryTests/When_DeleteProductAsync_Is_Called.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Test.Implementations.ProductInventoryTests
+{
+    internal class When_DeleteProductAsync_Is_Called
+    {
+
+    }
+}
