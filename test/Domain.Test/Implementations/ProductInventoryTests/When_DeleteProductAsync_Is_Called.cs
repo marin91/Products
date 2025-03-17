5302de8c191bce76966c2b06aa7a1d54a79d94cf
Miguel Marin <miguel91.macias@gmail.com>
2025-03-17 16:37:28 -0700

    Added test implementations for the test fixture that validates product removals

--- test/Domain.Test/Implementations/ProductInventoryTests/When_DeleteProductAsync_Is_Called.cs
+++ test/Domain.Test/Implementations/ProductInventoryTests/When_DeleteProductAsync_Is_Called.cs
@@ -2,13 +2,15 @@
 using Domain.Models;
 using FluentAssertions;
 using Domain.Exceptions;
+using Domain.Abstractions.Repositories;
+using Moq.Language.Flow;
 
 namespace Domain.Test.Implementations.ProductInventoryTests
 {
     internal class When_DeleteProductAsync_Is_Called : ProductInventoryTest
     {
 
-        long _productId;
+        private long _productId;
 
         [SetUp]
         public void Setup()
@@ -28,26 +30,70 @@ namespace Domain.Test.Implementations.ProductInventoryTests
             await act.Should().ThrowAsync<ProductDoesNotExistException>()
                 .WithMessage($"The product with {_productId} does not exist in the system.");
 
-
+            _mockProductRemover.Verify(x => x.DeleteProductAsync(It.IsAny<long>()), Times.Never);
         }
 
         [Test]
-        public void If_DeleteProductAsync_Throws_An_Exception_Then_It_Is_Caught_And_Rethrown()
+        public async Task If_DeleteProductAsync_Throws_An_Exception_Then_It_Is_Caught_And_Rethrown()
         {
+            var product = CreateFakeProduct(_productId);
 
+            SetupGetStoreProductByIdAsync(_productId).ReturnsAsync(product);
+
+
+            var someSqlDownstreamException = new Exception("Something occurred while removing the product in the data store.");
+
+            SetupDeleteProductAsync(_productId).ThrowsAsync(someSqlDownstreamException);
+
+            Func<Task> act = async () => { await _systemUnderTest.DeleteProductAsync(_productId); };
+
+            await act.Should().ThrowAsync<Exception>()
+                .WithMessage(someSqlDownstreamException.Message);
         }
 
         [Test]
         public void If_The_Product_Does_Exist_Then_It_Is_Deleted_Successfully()
         {
+            var product = CreateFakeProduct(_productId);
 
+            SetupGetStoreProductByIdAsync(_productId).ReturnsAsync(product);
+
+            Func<Task> act = async () => { await _systemUnderTest.DeleteProductAsync(_productId); };
+
+            act.Should().NotThrowAsync();
+
+            _mockProductRemover.Verify(x => x.DeleteProductAsync(_productId), Times.Once());
         }
       
         private static long GenerateProductId()
         {
             Random rnd = new Random();
 
+            return rnd.Next(10000);
+        }
+
+        private static int GenerateProductQty()
+        {
+            Random rnd = new Random(1000);
+
             return rnd.Next();
+        }
+
+        private static decimal GenerateProductPrice()
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(500);
+        }
+
+        private static Product CreateFakeProduct(long productId)
+        {
+            return new Product(productId, $"Some Description for {productId}", GenerateProductPrice(), GenerateProductQty());
+        }
+
+        protected ISetup<IRemoveProducts, Task> SetupDeleteProductAsync(long productId)
+        {
+            return _mockProductRemover.Setup(x => x.DeleteProductAsync(productId));
         }
 
     }
