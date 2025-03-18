fa7587c663e1d475580b59abe7f9baef03fdbc1c
Miguel Marin <miguel91.macias@gmail.com>
2025-03-17 17:18:38 -0700

    Added test fixture stubs

---
+++ test/Products.Api.Test/Controllers/ProductControllerTests/When_AddProduct_Is_Called.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Api.Test.Controllers.ProductControllerTests
+{
+    internal class When_AddProduct_Is_Called : ProductControllerTest
+    {
+    }
+}
