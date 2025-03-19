84ef4d8a849127e0363b697be6fb222eb5392ecd
Miguel Marin <miguel91.macias@gmail.com>
2025-03-18 23:13:01 -0700

    Introduced a new user defined exception

---
+++ src/Domain/Exceptions/CustomerAlreadyExistsException.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Exceptions
+{
+    public class CustomerAlreadyExistsException : Exception
+    {
+        public CustomerAlreadyExistsException() 
+        { 
+        
+        }
+
+        public CustomerAlreadyExistsException(string message) : base(message) 
+        { 
+        
+        }
+
+        public CustomerAlreadyExistsException(string message,  Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        public CustomerAlreadyExistsException(long productId) : base($"The product with {productId} already exists in the system.")
+        {
+
+        }
+    }
+}
