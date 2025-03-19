e98ae9172a46dbb23912c434f5f3bb3d1d07fc3d
Miguel Marin <miguel91.macias@gmail.com>
2025-03-18 23:01:47 -0700

    Added the Insert operation

--- src/Infrastructure.SqlServer/Implementations/Repositories/ProductWriter.cs
+++ src/Infrastructure.SqlServer/Implementations/Repositories/ProductWriter.cs
@@ -1,8 +1,10 @@
 ﻿using Domain.Abstractions.Repositories;
-using Domain.Models;
+using Domain.Exceptions;
 using Infrastructure.SqlServer.Options;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DomainProduct = Domain.Models.Product;
 
 namespace Infrastructure.SqlServer.Implementations.Repositories
 {
@@ -17,15 +19,55 @@ namespace Infrastructure.SqlServer.Implementations.Repositories
         }
 
         /// <inheritdoc />
-        public Task CreateProductAsync(Product product)
-        {
-           
+        public async Task CreateProductAsync(DomainProduct product)
+        {            
+            try
+            {
+                var productId = product.Id;
 
-            throw new NotImplementedException();
+                _logger.LogInformation($"Attempting to create a product with Id: {productId}");
+
+                using (SqlConnection conn = new SqlConnection(_connectionString))
+                {
+                    await conn.OpenAsync();
+
+                    string sql = "INSERT INTO Product (Id, Description, Price, Quantity) VALUES (@Id, @Description, @Price, @Quantity)";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Id", product.Id);
+                        cmd.Parameters.AddWithValue("@Description", product.Description);
+                        cmd.Parameters.AddWithValue("@Price", product.Price);
+                        cmd.Parameters.AddWithValue("@Quantity", product.Quantity);
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                        if (rowsAffected == 0)
+                        {
+                            throw new Exception("The query executed successfully but the product was not added to the table.");
+                        }
+
+                        _logger.LogInformation("The product was successfully created in the Product table.");
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                _logger.LogError(ex, "An error was raised from the SQL Server engine.");
+
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected issue occurred while creating a product.");
+
+                throw;
+            }
         }
 
         /// <inheritdoc />
-        public Task UpdateProductAsync(Product product)
+        public Task UpdateProductAsync(DomainProduct product)
         {
             throw new NotImplementedException();
         }
